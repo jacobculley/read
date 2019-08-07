8c32aac31b2b87545260d1008f8ff5a167cdc61c
Gurkirt <guru094@gmail.com>
2019-08-07 13:56:02 +0100

    added plot

--- plot_annots.py
+++ plot_annots.py
@@ -17,24 +17,14 @@ if not os.path.isdir(outdir):
 def main(annofile):
 
     base_name = annofile[:10]
-    video_imagedir = imagedir + '/' + base_name
     video_plot_dir = outdir + '/' + base_name
     vidfile = base_name[:-4]
-
-    if not os.path.isdir(video_imagedir):
-        os.makedirs(video_imagedir)
     
     if not os.path.isdir(video_plot_dir):
         os.makedirs(video_plot_dir)
     
-    imglist = os.listdir(video_imagedir)
-    imglist = [i for i in imglist if i.endswith('.jpg')]
-    
-    if len(imglist) < 2000 and os.path.isfile(vidfile):
-        cmd = 'ffmpeg -i {} -qscale:v 15 {}%05d.jpg'.format(vidfile, video_imagedir)
-        print(cmd)
-            # f.write(cmd+'\n') ## this could act a log file
-        os.system(cmd)
+    video_handle = cv2.VideoCapture(vidfile)
+    # video_writter_object =  cv2.VideoWriter()
 
     with open(annofile,'r') as f:
         db = json.load(f)
@@ -48,12 +38,16 @@ def main(annofile):
     for f in sorted(frames.keys()):
         print(f)
         in_id = int(f)
-        input_image_path = '{:s}/{:05d}.png'.format(imagedir, 3*(in_id-1)+1)
-        import cv2
+        # input_image_path = '{:s}/{:05d}.png'.format(imagedir, 3*(in_id-1)+1)
+        # import cv2
 
         color = (0, 0, 255)
         color1 = (255, 0, 5)
-        frame = cv2.imread(input_image_path)
+
+        flag, frame = video_handle.retrive()
+        
+        for i in range(3):
+            _, _ = video_handle.retrive()
 
         annots = frames[f]
         for anno in annots:
